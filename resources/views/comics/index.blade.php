cd29869b933ce38ec3a5eef465a5471a95f6bac6
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-05-24 15:54:35 +0200

    feat: add show all the images on page index of folder comics using CRUD

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Comics Index Page</h2>    
+
+    <section class="py-4">
+        <div class="container">
+            <h2 class="text-center">Tutti i fumetti DC</h2>
+            <div class="row">
+                @foreach ($comics as $comic)
+                    <div class="col-4 my-3">
+                        <div class="card">
+                            <div class="card-body">
+                            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                            <h5 class="card-title">{{ $comic->title }}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">{{ $comic->price }}</h6>
+                            <p class="card-text">{{ $comic->series }}</p>
+                            <button class="btn btn-primary">Scropri di più</button>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                
+            </div>    
+        </div>
+    </section>
+    
 @endsection
