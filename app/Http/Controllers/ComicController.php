50b8af893f4e7d4773199d77cf4ac33a89d23278
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-05-24 17:20:08 +0200

    feat: changed the order of the cards in the index of comics to be descendent

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -15,7 +15,7 @@ class ComicController extends Controller
     public function index()
     {
 
-        $comic = Comic::all();
+        $comic = Comic::orderBy('id', 'desc')->get();
 
         $data = [
             'comics' => $comic
